e70424295eb2ad7d38cc1fe1f8294c6e050d7e5c
Raizal I.N. Pregnanta <raizal.pregnanta@gmail.com>
2022-04-02 18:13:58 +0700

    [UPDATE] multi password for amd64 & arm64 install.sh

--- arm64/monitor/install.sh
+++ arm64/monitor/install.sh
@@ -2,10 +2,26 @@
 
 chmod +x *.sh armbian-monitor
 
-echo "Masukkan password untuk root STB anda: "
-read password
+passwords=""
+more_password="y"
+
+while [ "$more_password" = "y" ]
+do
+	echo "Masukkan password untuk root STB anda: "
+	read password
+	
+	if [ ! "$passwords" ];then
+		passwords="'$password'"
+	else
+		passwords="$passwords, '$password'"
+	fi
+	
+	echo "Ada password lain? (y/n) : "
+	read more_password
+done
+
 echo "STB_USERNAME=root
-STB_PASSWORD=$password" > .env
+STB_PASSWORD=\"[$passwords]\"" > .env
 
 echo "Install armbian-monitor.serivce"
 cp ./armbian-monitor.service /etc/systemd/system/
